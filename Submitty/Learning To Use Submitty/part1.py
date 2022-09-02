a75decbd2ce113267ab3390604f836d312a5ca32
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-09-02 09:15:17 -0400

    Labels and Main()

--- Submitty/Learning To Use Submitty/part1.py
+++ Submitty/Learning To Use Submitty/part1.py
@@ -4,22 +4,30 @@
 # This program calculates Body Mass Index from height and weight inputted by user
 #########################
 
-# Get the user input
-weight = eval(input("Enter your weight in pounds: "))
-print(weight)
-height_ft = eval(input("Enter your height in feet: "))
-print(height_ft)
-height_in = eval(input("Enter your remaining height in inches: "))
-print(height_in)
+#Initialize
+def main():
+    # Get the user input
+    weight = eval(input("Enter your weight in pounds: "))
+    print(weight)
+    height_ft = eval(input("Enter your height in feet: "))
+    print(height_ft)
+    height_in = eval(input("Enter your remaining height in inches: "))
+    print(height_in)
 
-# Converting and combining the two height inputs
-height = (height_ft * 12) + height_in
+    # Process
+    # Converting and combining the two height inputs
+    height = (height_ft * 12) + height_in
 
-# Calculating the BMI
-BMI = (weight / height**2) * 703
+    # Calculating the BMI
+    BMI = (weight / height**2) * 703
 
-# Round to two decimal places
-BMI = round(BMI, 2)
+    # Round to two decimal places
+    BMI = round(BMI, 2)
 
-#Return the value
-print("Your BMI is", BMI)
+    #Output
+    #Return the value
+    print("Your BMI is", BMI)
+
+    # Terminate
+
+main()
--- Submitty/Learning To Use Submitty/part2.py
+++ Submitty/Learning To Use Submitty/part2.py
@@ -4,28 +4,35 @@
 # This program calculates six functions using two user-inputted numbers: add, subtract, multiply, integer division, division, and modulus
 #########################
 
-# Get the user input
-in_1 = eval(input("Enter a value: "))
-print(in_1)
-in_2 = eval(input("Enter a second value: "))
-print(in_2)
+# Initialize
+def main():
+    # Get the user input
+    in_1 = eval(input("Enter a value: "))
+    print(in_1)
+    in_2 = eval(input("Enter a second value: "))
+    print(in_2)
 
-# Print the calculated values
+    # Process and Output
+    # Print the calculated values
 
-# Addition
-print(in_1, "+", in_2, "=", in_1 + in_2)
+    # Addition
+    print(in_1, "+", in_2, "=", in_1 + in_2)
 
-# Subtraction
-print(in_1, "-", in_2, "=", in_1 - in_2)
+    # Subtraction
+    print(in_1, "-", in_2, "=", in_1 - in_2)
 
-# Multiplication
-print(in_1, "*", in_2, "=", in_1 * in_2)
+    # Multiplication
+    print(in_1, "*", in_2, "=", in_1 * in_2)
 
-# Integer divison
-print(in_1, "//", in_2, "=", in_1 // in_2)
+    # Integer divison
+    print(in_1, "//", in_2, "=", in_1 // in_2)
 
-# Division
-print(in_1, "/", in_2, "=", in_1 / in_2)
+    # Division
+    print(in_1, "/", in_2, "=", in_1 / in_2)
 
-# Modulus
-print(in_1, "%", in_2, "=", in_1 % in_2)
+    # Modulus
+    print(in_1, "%", in_2, "=", in_1 % in_2)
+
+    #Terminate
+
+main()
