7289ec68f48571e5b175a62937ee9131133f2ab0
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-09-03 14:43:41 -0400

    File adjustment

---
+++ [Project] Are You Ready/README.md
@@ -0,0 +1,4 @@
+Author: Lex Baker
+Date: 9/3/2022
+Purpose: To calculate how ready someone is for a certain event, given several factors.
+
---
+++ [Project] Are You Ready/main.py
@@ -0,0 +1,37 @@
+#########################
+# Author: Lex Baker
+# Date: 9/3/22
+# This program takes several user inputs and calculates how prepared they are for an upcoming event
+#########################
+
+def main():
+    # Input
+    # Variables S, C, and P_N are used in the numerator calculations
+    # Hours of sleep
+    S = eval(input("Hours of sleep you had last night: "))
+    print(S)
+    # Shots of stimulants
+    C = eval(input("Shots of espresso or other stimulants consumed: "))
+    print(C)
+    # Hours of prep needed for the event
+    P_N = eval(input("Hours of preparation needed to excel: "))
+    print(P_N)
+    # Variables P_A, D, N, and I are used in the denominator calculations
+    # Hours spent preparing
+    P_A = eval(input("Hours you actually spent preparing: "))
+    print(P_N)
+    # Difficulty of subject matter
+    D = eval(input("Difficulty of the subject matter (1 - 10 with 10 being \'theoretical particle physics\'): "))
+    print(D)
+    #Level of nervousness
+    N = eval(input("Level of nervousness ( 1 - 10 with 10 being \'tightrope across the grand Canyon on a windy day\'): "))
+    print(N)
+    # Importance of Event (1-10)
+    I = eval(input("Importance of the event (1 - 10 with 10 being \'singing the national anthem at the Super Bowl\'): "))
+    print(I)
+
+    # Processing
+    
+
+
+main()
