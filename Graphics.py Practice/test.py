3e2d037158390b0b83cd4290d156cdd7f2a9ac71
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-09-19 09:33:44 -0400

    Got graphics.py working

---
+++ Graphics.py Practice/test.py
@@ -0,0 +1,22 @@
+
+import sys
+import os
+ 
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+from graphics import *
+
+def main():
+    win = GraphWin("My Circle", 300, 300)
+    leftEye = Circle(Point(80, 50), 5)
+    leftEye.setFill('yellow')
+    leftEye.setOutline('red')
+    rightEye = leftEye.clone() # rightEye is exact copy of the left
+    rightEye.move(20, 0)
+    leftEye.draw(win)
+    rightEye.draw(win)
+    win.getMouse() #pause
+    win.close() #close
+
+main()
