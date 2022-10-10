3ccef9c05c295d579807cf4e8084ffe4a5675911
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-10-10 09:39:36 -0400

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -165,7 +165,8 @@ import time, os, sys
 try:  # import as appropriate for 2.x vs. 3.x
    import tkinter as tk
 except:
-   import Tkinter as tk
+   #import Tkinter as tk
+   pass
 
 
 ##########################################################################
