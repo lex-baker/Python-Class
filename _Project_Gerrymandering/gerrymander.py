84cd6b68c1e0d38c00dd0b9abb5304343b85c186
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-10-17 11:08:18 -0400

    Update gerrymander.py

--- _Project_Gerrymandering/gerrymander.py
+++ _Project_Gerrymandering/gerrymander.py
@@ -1,1 +1,13 @@
+from re import S
 from graphics import *
+
+state = input("Enter a state to find if it's gerrymandered: ").lower()
+
+districts = open("districts.txt")
+districts = districts.readlines()
+
+for line in districts:
+    s = line.split(",")[0].lower()
+    if s == state:
+        print(line)
+print(state + " is not a state")
