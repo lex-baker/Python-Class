cec3f045ce416d47a0221cde9632e0e8137c48ee
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-10-25 15:11:46 -0400

    Update gerrymander.py

--- _Project_Gerrymandering/gerrymander.py
+++ _Project_Gerrymandering/gerrymander.py
@@ -1,13 +1,37 @@
 from re import S
 from graphics import *
 
-state = input("Enter a state to find if it's gerrymandered: ").lower()
+districts = open("districts.txt").readlines()
 
-districts = open("districts.txt")
-districts = districts.readlines()
+voters = open("eligible_voters.txt").readlines()
 
-for line in districts:
-    s = line.split(",")[0].lower()
-    if s == state:
-        print(line)
-print(state + " is not a state")
+def main():
+    input_state = input("Enter a state to find if it's gerrymandered: ").lower()
+    district_info = None
+    voter_info = None
+    for line in districts:
+        state_name = line.split(",")[0].lower()
+        if state_name == input_state:
+            district_info = line
+            print(district_info)
+            break
+    
+    for line in voters:
+        state_name = line.split(",")[0].lower()
+        if state_name == input_state:
+            voter_info = line
+            print(voter_info)
+            break
+
+    if district_info == None or voter_info == None: 
+        print(input_state + " is not a state")
+    else:
+        gerrymander(voter_info[0], voter_info[1], district_info)
+
+
+def gerrymander(state, num_voters, state_district):
+    print()
+    win = GraphWin("Title", )
+
+
+main()
