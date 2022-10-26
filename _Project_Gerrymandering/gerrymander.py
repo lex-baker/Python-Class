d6feca3f359f0d8d8f3cb579200e536d3b8bb3f3
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-10-25 20:00:20 -0400

    Update gerrymander.py

--- _Project_Gerrymandering/gerrymander.py
+++ _Project_Gerrymandering/gerrymander.py
@@ -10,13 +10,27 @@ voters = open("eligible_voters.txt").readlines()
 
 def main():
     input_state = input("Enter a state to find if it's gerrymandered: ")
-    district_info = get_district_info(input_state)
-    voter_info = get_voter_info(input_state)
-    if district_info == None or voter_info == None: 
-        print(input_state + " is not a state")
+    print(input_state)
+    raw_district_info = get_district_info(input_state)
+    if raw_district_info != None:
+        state_name = raw_district_info.split(",")[0]
+        total_voters = get_voter_info(input_state)
+        #is_gerrymandered(voter_info[0], voter_info[1], district_info)
+        #print information
+        #draw graph
         
     else:
-        is_gerrymandered(voter_info[0], voter_info[1], district_info)
+        print(f"\"{input_state}\" not found.")
+        
+
+    """
+    Which state do you want to look up? Arizona
+    Total Wasted Democratic votes: 327852
+    Total Wasted Republican votes: 369697
+    4738332 eligible voters
+    The Efficiency Gap is 0.9% in favor of the Democrats.
+    The State does not appear to be gerrymandered.
+    """
 
 def get_district_info(user_input):
     district_info = None
@@ -26,14 +40,17 @@ def get_district_info(user_input):
         if state_name == user_input.lower():
             district_info = line
             print(district_info)
-            break
+            return district_info
+    return None
+
 def get_voter_info(user_input):
     for line in voters:
         state_name = line.split(",")[0].lower()
         if state_name == user_input.lower():
-            voter_info = line
+            voter_info = line.split(",")[1]
             print(voter_info)
-            break
+            return voter_info
+    return None
 
 def is_gerrymandered(state, num_voters, state_district):
     print()
