bf432c072af0c0120e20aa9993d1440570a30622
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-10-31 14:03:40 -0400

    Create racketball.py

---
+++ In Class Code/racketball.py
@@ -0,0 +1,9 @@
+def main():
+    playerA = float(input("Enter the first player's chance of winning (in decimal): "))
+    playerB = float(input("Enter the second player's chance of winning (in decimal): "))
+    numGames = int(input("Enter the number of games to simulate: "))
+    
+
+
+if __name__ == "__main__":
+    main()
