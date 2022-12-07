51a62642a57d895ceecdf54dcc42c488e5d76983
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-12-07 13:21:30 -0500

    Update

--- Sorting Presentation/bogosort.py
+++ Sorting Presentation/bogosort.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import random
 import os
 from time import perf_counter
+import numpy as np
 
 def is_sorted(arr):
     # Find time complexity for built-in sorted and manual sort checking
@@ -28,10 +29,15 @@ def test():
     # array = open("datasets/data100.txt").readlines()
     # array = [int(x) for x in array]
 
+    outfile = open('5_20_results.txt', 'w')
+
+    data = []
+
     # Even smaller auto test cases (size of 5 through 10)
-    for i in range(5, 16):
-        # Create random values in array
-        array = [random.randrange(30) for j in range(i)]
+    for i in range(5, 21):
+        # Create iterative values in array and randomize
+        array = [j for j in range(i)]
+        random.shuffle(array)
 
         # time start
         time_start = perf_counter()
@@ -45,10 +51,69 @@ def test():
         # calc time
         time_elapsed = time_stop - time_start
 
-        print("For array of size", len(array))
+        # Print to terminal
+        print("For array of size:", len(array))
         print("Time elapsed:", time_elapsed, "seconds")
         print("Total iterations:", iterations_needed)
         print("\n", "-"*30, "\n", sep="")
+
+        # Print to outfile
+        print("For array of size:", len(array), file=outfile)
+        print("Time elapsed:", time_elapsed, "seconds", file=outfile)
+        print("Total iterations:", iterations_needed, file=outfile)
+        print("\n", "-"*30, "\n", sep="", file=outfile)
+
+        data.append( [len(array), time_elapsed, iterations_needed] )
+
+    print(data)
+    print(data, file=outfile)
+
+    outfile.close()
+
+    plt.rcParams['axes.autolimit_mode'] = 'round_numbers'
+
+    f, (ax1, ax2) = plt.subplots(1, 2, sharex=True, figsize=(10, 5)) 
+
+    f.suptitle("Bogosort Efficiency by Time and Iterations")
+
+    # Plotting time
+    ax1.plot(
+        [ x[0] for x in data ], # x-axis
+        [ y[1] for y in data ], # y-axis
+        "-x"
+    )
+
+    ax1.set_title('Time in Seconds')
+    ax1.set_xlabel("Array Size")
+    ax1.set_xlim(data[0][0], data[-1][0])
+    ax1.set_ylim(0, data[-1][1])
+    ax1.set_box_aspect(1)
+
+
+    # Plotting iterations
+    ax2.plot(
+        [ x[0] for x in data ], # x-axis
+        [ y[2] for y in data ], # y-axis
+        "-x"
+    )
+
+    ax2.set_title('Iterations')
+    ax2.set_xlabel("Array Size")
+    # only have to do this once b/c sharex is True
+    ax2.set_xticks(np.arange(data[0][0], data[-1][0] + 1, 1))
+    #ax2.set_yticks(np.arange(data[0][2], data[-1][2] + 1))
+    ax2.set_xlim(data[0][0], data[-1][0])
+    ax2.set_ylim(0, data[-1][2] + 1)
+    ax2.set_box_aspect(1)
+
+    plt.subplots_adjust(
+        wspace=0.5
+    )
+
+    plt.savefig('5_20_results.png')
+    plt.show()
+
+    
 
 
     # remove this later
